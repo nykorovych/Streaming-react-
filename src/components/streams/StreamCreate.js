be25010a59334a6053236350d815f82a48b5db0d
Iurii <ny.iuriii@gmail.com>
2020-08-03 17:41:58 +0200

    lodat pich this strems parenthesis title parenthesis

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,77 +1,39 @@
 import React from "react";
-import { Field, reduxForm } from "redux-form";
-import { connect } from 'react-redux'
-import { createStream } from '../../actions'
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
+import StreamForm from "../StreamForm";
 
 class StreamCreate extends React.Component {
-  
-  //   renderInput(formProps) {
-  //     console.log(formProps); ?__________________________________
-  //     return (
-  //       <input
-  //         onChange={formProps.input.onChange}
-  //         value={formProps.input.value}
-  //       ></input>
-  //     );
-  //   }
-  renderError = (formProps) =>{
-    if( formProps.meta.touched && formProps.meta.error) {
-        return (
-            <div className="ui error message">
-                <div className="header">{formProps.meta.error} </div>
-            </div>
-        )
-    }
-  }
-  renderInput = (formProps) => {
-      const className = `field ${formProps.meta.error && formProps.meta.touched ? 'error' : ''}`
-    return (
-
-      <div className={className}>
-        <label>{formProps.label}</label>
-        <input {...formProps.input} autoComplete="off"/>
-       {this.renderError(formProps)}
-      </div>
-    );
-  }
   onSubmit = (formValues) => {
-    this.props.createStream(formValues)
-  }
+    this.props.createStream(formValues);
+  };
   render() {
     return (
-      <form
-        onSubmit={this.props.handleSubmit(this.onSubmit)}
-        className="ui form error"
-      >
-        <Field
-          name="title"
-          component={this.renderInput}
-          label="Enter a name"
-        ></Field>
-        <Field
-          name="description"
-          component={this.renderInput}
-          label="Enter a description"
-        ></Field>
-        <button className="ui button primary">Submit</button>
-      </form>
+      <div>
+        <h3>Create a stream</h3>
+        <StreamForm onSubmit={this.onSubmit}></StreamForm>
+      </div>
     );
   }
 }
 
-const validate = (formValues) => {
-  const errors = {};
-  if (!formValues.title) {
-    errors.title = " Enter  a title";
-  }
-  if (!formValues.description) {
-    errors.description = "Must be a description";
-  }
-  return errors;
-};
-const reduxFormWraped =  reduxForm({
-  form: "streamCreate",
-  validate: validate,
-})(StreamCreate);
+export default connect(null, { createStream })(StreamCreate);
 
-export default connect(null, {createStream} )(reduxFormWraped)
+//   renderInput(formProps) {
+//     console.log(formProps); ?__________________________________
+//     return (
+//       <input
+//         onChange={formProps.input.onChange}
+//         value={formProps.input.value}
+//       ></input>
+//     );
+//   }
+// renderError = (formProps) =>{
+//   if( formProps.meta.touched && formProps.meta.error) {
+//       return (
+//           <div className="ui error message">
+//               <div className="header">{formProps.meta.error} </div>
+//           </div>
+//       )
+//   }
+// }
--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React from "react";
+import _ from 'lodash'
+import { connect } from "react-redux";
+import { fetchStream, editStream } from "../../actions/";
+import StreamForm from '../../components/StreamForm'
 
-const StreamEdit = (props) => {
-    console.log(props)
-    return <div>StreamEdit</div>
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+  onSubmit = (formvalues) => {
+    console.log(formvalues)
+  }
+  render() {
+    if(!this.props.stream) {
+      return <div>Loading...</div>
+    }
+    return (
+      <div>
+        <h3>Edit Stream</h3>
+        <StreamForm initialValues={_.pick(this.props.stream, 'title', 'description')} onSubmit={this.onSubmit}></StreamForm>
+      </div>
+    )
+  }
 }
-
-export default StreamEdit
+const mstp = (state, ownProps) => {
+  console.log(state);
+  return { stream: state.stream[ownProps.match.params.id] };
+};
+export default connect(mstp, { fetchStream, editStream })(StreamEdit);
