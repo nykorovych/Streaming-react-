21401a3e3995c55e928aa1d6df1866ed7676c9ed
Iurii <ny.iuriii@gmail.com>
2020-08-05 12:49:27 +0200

    after

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -10,7 +10,7 @@ import { fetchStream }from '../../actions'
      
     render() {
         if(!this.props.stream) {
-            return <div>LOading</div>
+            return <div>loading</div>
         }
         return (
             <div>
